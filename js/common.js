ce9c8dfdab8cc91684f5298d5e8a8ce63ea0d08a
youjianghe <youjianghe@foxmail.com>
2017-07-31 11:48:26 +0800

    更新

--- js/common.js
+++ js/common.js
@@ -3,15 +3,35 @@ $(function($){
 	$('.mnav').on('click',function(){
 		$('#classify').toggleClass('active hidden-xs hidden-sm');
 	});
+    var href = window.location.hash;//获取链接地址后面#的内容
+    if($(href).length<=0){href = '#mylinks_link'}//初始化找不到ID的到首页
+    toggleLink($('#classify a[href="'+href+'"]'));
 
 	$('#classify a').click(function (e) {
-  		e.preventDefault();
-  		$(this).tab('show');
-  		$('#classify').removeClass('active hidden-xs hidden-sm');
+       toggleLink($(this));
 	});
+    $('#vframe').contents().find('body').append('<p style="color:#ffffff;">请在上面输入视频地址，并点击播放，vip视频也能看</p>')
+    playVideo();
 });
 
+function playVideo(){
+     $('.btn-play').click(function(){
+            var api = $('#selecturl option:selected').val();
+            var videourl = $('input[name = "videourl"]').val();
+            $('#vframe').attr('src',api+videourl);
+     });
 
+}
+
+function toggleLink(href){
+    var link = href.attr('href').split('_')[1];
+    if(link =="link"){
+        $('.nav-tabs li').removeClass('active');
+        href.parent().addClass('active');
+        $('.tab-content .tab-pane').removeClass('active');
+        $(href.attr('href')).addClass('active');
+    }
+}
 function addFavorite2() {
     var url = window.location;
     var title = document.title;
